cfa9bb90c85454ae6694cf59e780bdcbf19287f2
Jim Zhang <jim.zhang@kaleido.io>
2024-07-02 13:31:58 -0400

    Replace MIT license with Apache-2.0 for Solidity files
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- solidity/contracts/zeto_anon_enc_nullifier.sol
+++ solidity/contracts/zeto_anon_enc_nullifier.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: MIT
+// SPDX-License-Identifier: Apache-2.0
 pragma solidity ^0.8.20;
 
 import {Groth16Verifier_AnonEncNullifier} from "./lib/verifier_anon_enc_nullifier.sol";
