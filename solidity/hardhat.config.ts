ef8773df82fddd82563cff616ac21284c569ca54
Jim Zhang <jim.zhang@kaleido.io>
2024-09-06 13:58:43 -0400

    disable optimizer to allow proxy deploy
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- solidity/hardhat.config.ts
+++ solidity/hardhat.config.ts
@@ -32,7 +32,7 @@ const config: HardhatUserConfig = {
     version: "0.8.20",
     settings: {
       optimizer: {
-        enabled: true,
+        enabled: false,
         runs: 1000,
       },
     },
