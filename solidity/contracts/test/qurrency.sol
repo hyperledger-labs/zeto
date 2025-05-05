d38bd25866a67bdf349c29afbe282336358b1ddc
Jim Zhang <jim.zhang@kaleido.io>
2025-05-05 17:07:52 -0400

    add gas cost test for qurrency proof verification
    Signed-off-by: Jim Zhang <jim.zhang@kaleido.io>

--- solidity/contracts/test/qurrency.sol
+++ solidity/contracts/test/qurrency.sol
@@ -1,21 +1,21 @@
 // SPDX-License-Identifier: MIT
 pragma solidity ^0.8.27;
 
-import {Groth16Verifier_Qurrency} from "../verifiers/verifier_qurrency.sol";
+import {Groth16Verifier_AnonNullifierQurrencyTransfer} from "../verifiers/verifier_anon_nullifier_qurrency_transfer.sol";
 import {Commonlib} from "../lib/common.sol";
 
 contract TestVerifierQurrency {
-    Groth16Verifier_Qurrency verifier;
+    Groth16Verifier_AnonNullifierQurrencyTransfer verifier;
 
     bool public lastSuccess = false;
 
     constructor() {
-        verifier = new Groth16Verifier_Qurrency();
+        verifier = new Groth16Verifier_AnonNullifierQurrencyTransfer();
     }
 
     function verifyProof(
         Commonlib.Proof memory proof,
-        uint256[50] memory publicInputs
+        uint256[1] memory publicInputs
     ) public returns (bool) {
         bool result = verifier.verifyProof(
             proof.pA,
