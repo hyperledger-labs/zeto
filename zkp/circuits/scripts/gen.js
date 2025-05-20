675d8cfced6b847968bcfa7298a53f0d0b31f7a7
Matthew Gregoire <matthew.gregoire@kaleido.io>
2025-05-20 12:53:04 -0400

    Moved gen script and related files to circuits/scripts
    Signed-off-by: Matthew Gregoire <matthew.gregoire@kaleido.io>

--- zkp/circuits/scripts/gen.js
+++ zkp/circuits/scripts/gen.js
@@ -106,7 +106,7 @@ const log = (circuit, message) => {
 
 // main circuit process logic
 const processCircuit = async (circuit, ptau, skipSolidityGenaration) => {
-  const circomInput = path.join("./", `${circuit}.circom`);
+  const circomInput = path.join("../", `${circuit}.circom`);
   const ptauFile = path.join(ptauDownload, `${ptau}.ptau`);
   const zkeyOutput = path.join(provingKeysRoot, `${circuit}.zkey`);
 
@@ -230,6 +230,7 @@ const processCircuit = async (circuit, ptau, skipSolidityGenaration) => {
 
   log(circuit, `Generating solidity verifier`);
   const solidityFile = path.join(
+    "..",
     "..",
     "..",
     "solidity",
