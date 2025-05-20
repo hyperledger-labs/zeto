5991af9ee3c4247b093433426db1560bd7fbf4c1
Matthew Gregoire <matthew.gregoire@kaleido.io>
2025-05-20 11:33:05 -0400

    Changed gen script concurrency default to match readmes
    Signed-off-by: Matthew Gregoire <matthew.gregoire@kaleido.io>

--- zkp/circuits/gen.js
+++ zkp/circuits/gen.js
@@ -45,7 +45,7 @@ const ptauDownload = process.env.PTAU_DOWNLOAD_PATH || argv.ptauDownloadPath;
 const specificCircuits = argv.c;
 const verbose = argv.v;
 const compileOnly = argv.compileOnly;
-const parallelLimit = parseInt(process.env.GEN_CONCURRENCY, 10) || 4; // Default to compile 4 circuits in parallel
+const parallelLimit = parseInt(process.env.GEN_CONCURRENCY, 10) || 10; // Default to compile 10 circuits in parallel
 
 // check env vars
 if (!circuitsRoot) {
