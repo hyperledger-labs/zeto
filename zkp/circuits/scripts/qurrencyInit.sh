69b5bb6a07aa44c6d8ab78e17775eecd1c54453d
Matthew Gregoire <matthew.gregoire@kaleido.io>
2025-05-27 11:43:04 -0400

    Added script to generate qurrency keys
    Signed-off-by: Matthew Gregoire <matthew.gregoire@kaleido.io>

---
+++ zkp/circuits/scripts/qurrencyInit.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+git clone https://github.com/dajiaji/crystals-kyber-js.git
+cd crystals-kyber-js
+
+sed -i '' 's/private _sampleMatrix[(]/public _sampleMatrix(/g' src/mlKemBase.ts
+sed -i '' 's/function polyFromBytes[(]a: Uint8Array[)]: Array<number> [{]/export function polyFromBytes(a: Uint8Array): Array<number> {/g' src/mlKemBase.ts
+
+cp ../generateQurrencyKey.ts ./generateQurrencyKey.ts
+
+deno run generateQurrencyKey.ts
+
+cd ..
+rm -rf crystals-kyber-js
